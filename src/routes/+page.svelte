dded44f4c447e65fd6ff90531a4291f058dcfb07
kee-siang <tksiang.ss@example.com>
2024-01-12 17:12:00 +0800

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+
+<div class="w-screen h-screen bg-gradient-to-br from-indigo-200 via-fuchsia-200 to-blue-200">
+    <img class="h-16 w-16"src="logo.png" alt="..."/>
+    <div class="flex">
+        <h1 id="title" class="h-48 text-center text-4xl my-16 ml-64 font-bold">Hi My name is Siang. A passionate young <span class="text-violet-800">web developer</span> that loves <span class="text-indigo-800">creating</span>, <span class="text-fuchsia-800">imaginating</span> and <span class="text-pink-700">coding things</span> to make them <span class="text-rose-800">real, accessible.</span></h1>
+        <img class="mr-10 h-96 w-96" src="profile-image.png" alt="..."/>
+    </div>
+
+    <div class="flex justify-center items-end h-24">
+        <button class="p-4 rounded-s-full rounded-e-full backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl font-semibold">
+            <span class="p-4 text-lg hover:bg-gradient-to-br from-indigo-200 via-fuchsia-200 to-blue-200 rounded-full">Home</span>
+            <span class="p-4 text-lg hover:bg-gradient-to-br from-teal-300 via-bg-indigo-400 to-purple-200 rounded-full">Portfolio</span>
+            <span class="p-4 text-lg hover:bg-gradient-to-br from-yellow-200 via-rose-300 to-orange-200 rounded-full">About</span>
+            <span class="p-4 text-lg hover:bg-gradient-to-r from-indigo-200 to-purple-200 rounded-full">Contact</span>
+        </button>
+    </div>
+</div>
+
+<style>
+    #title{
+        width: 400px;
+    }
+
+</style>

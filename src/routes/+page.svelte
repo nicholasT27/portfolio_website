f533f1a752370f282c830837dcbdac8d8f40f2a6
kee-siang <tksiang.ss@example.com>
2024-01-18 12:27:30 +0800

    add sliding up and down effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,12 +20,12 @@
 
 <div class="w-screen h-screen bg-gradient-to-br from-indigo-200 via-fuchsia-200 to-blue-200">
     <img class="h-16 w-16"src="logo.png" alt="..."/>
-    <div class="flex">
+    <div class="flex slide-down">
         <h1 id="title" class="h-48 text-center text-4xl my-16 ml-64 font-bold">Hi My name is Siang. A passionate young <span class="text-violet-800">web developer</span> that loves <span class="text-indigo-800">creating</span>, <span class="text-fuchsia-800">imaginating</span> and <span class="text-pink-700">coding things</span> to make them <span class="text-rose-800">real, accessible.</span></h1>
         <img class="mr-10 h-96 w-96" src="profile-image.png" alt="..."/>
     </div>
 
-    <div class="flex justify-center items-end h-24 relative hs-tooltip inline-block">
+    <div class="flex justify-center items-end h-10 mt-8 relative hs-tooltip inline-block top-1 slide-up">
         <button class="p-4 rounded-s-full rounded-e-full backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl font-semibold">
             <span class="p-4 text-lg hover:bg-gradient-to-br from-indigo-200 via-fuchsia-200 to-blue-200 rounded-full">Home</span>
             <span class="p-4 text-lg hover:bg-gradient-to-br from-teal-300 via-bg-indigo-400 to-purple-200 rounded-full">Portfolio</span>
@@ -34,7 +34,7 @@
         </button>
 
 
-        <audio src="background-music.mp3" autoplay loop bind:this={audio}></audio>
+        <audio src="background-music.mp3" loop bind:this={audio}></audio>
 
          <button on:click={togglePlayPause} class="hs-tooltip-toggle absolute right-6 p-2 rounded-lg backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl">
             {#if $play}

11b9c845557edb7af752d168d6dc3db203dd2f12
kee-siang <tksiang.ss@example.com>
2024-01-23 11:26:41 +0800

    create first section layout

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,19 @@
+<div class="bg-gradient-to-br from-orange-200 via-rose-300 to-yellow-200">
+    <img class="h-16 w-16"src="logo.png" alt="..."/>
+    <h1 class="text-9xl font-semibold mt-20 ml-10">
+        About
+    </h1>
+
+    <div class="w-10 text-center ml-44 mt-24 font-semibold">Info</div>
+    <div class="flex justify-center">
+    <h1 class="intro text-2xl font-bold pt-10 mr-10 backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl p-5 rounded-lg">A web developer passionate about creating <span class="text-fuchsia-700">clean</span> and <span class="text-pink-600">intuitive user experiences</span>. Proficient in <span class="text-blue-600">HTML</span>, <span class="text-teal-600">CSS</span>, and <span class="text-lime-600">JavaScript</span>, I enjoy turning ideas into interactive websites. Eager to learn and contribute to dynamic projects, I'm excited about the possibilities in <span class="text-purple-500">web development</span> !!!</h1>
+    <img src="profile-image-2.png" class="w-96 h-96" alt=""/>
+    </div>
+</div>
+
+<style>
+    .intro{
+        width: 500px;
+        height: 350px;
+    }
+</style>

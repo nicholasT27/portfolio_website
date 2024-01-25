c718b1e96d81eefdca17d17dbb241cf61f26d524
kee-siang <tksiang.ss@example.com>
2024-01-25 22:59:30 +0800

    do second and third section of the about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,8 +6,43 @@
 
     <div class="w-10 text-center ml-44 mt-24 font-semibold">Info</div>
     <div class="flex justify-center">
-    <h1 class="intro text-2xl font-bold pt-10 mr-10 backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl p-5 rounded-lg">A web developer passionate about creating <span class="text-fuchsia-700">clean</span> and <span class="text-pink-600">intuitive user experiences</span>. Proficient in <span class="text-blue-600">HTML</span>, <span class="text-teal-600">CSS</span>, and <span class="text-lime-600">JavaScript</span>, I enjoy turning ideas into interactive websites. Eager to learn and contribute to dynamic projects, I'm excited about the possibilities in <span class="text-purple-500">web development</span> !!!</h1>
-    <img src="profile-image-2.png" class="w-96 h-96" alt=""/>
+        <h1 class="intro text-2xl font-bold pt-10 mr-10 backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl p-5 rounded-lg">A web developer passionate about creating <span class="text-fuchsia-700">clean</span> and <span class="text-pink-600">intuitive user experiences</span>. Proficient in <span class="text-blue-600">HTML</span>, <span class="text-teal-600">CSS</span>, and <span class="text-lime-600">JavaScript</span>, I enjoy turning ideas into interactive websites. Eager to learn and contribute to dynamic projects, I'm excited about the possibilities in <span class="text-purple-500">web development</span> !!!</h1>
+        <img src="profile-image-2.png" class="w-96 h-96" alt=""/>
+    </div>
+
+    <div class="flex justify-center mt-24">
+        <img src="profile-image-4.png" class="w-80 h-80 mt-8" alt="" />
+        <div class="flex flex-col">
+        <div class="w-10 text-center ml-6 font-semibold">Education</div>
+        <h1 class="intro-1 text-2xl font-bold pt-10 mr-10 backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl p-5 rounded-lg flex flex-col">
+            Next Academy - Full Stack Web Development
+            <div class="flex mt-5">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right text-orange-600 mt-1"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>
+                <a href="https://drive.google.com/file/d/1TC5cQ-ors8yiV2AKKXMkgwWDJolJhBmW/view?usp=drive_link" target="_blank" class="ml-1 font-semibold text-base mt-0.5 hover:text-red-500">Cert</a>
+            </div>
+
+            <div class="mt-10">
+                <span class="mt-10">Wawasan Open University</span>
+                <div class="mt-2 text-base capitalize">bachelor of information technology (honours) multimedia systems</div>
+                <span class="mt-2 text-sm capitalize">major in multimedia and web development</span>
+                <div class="mt-2 text-xs">2019 - 2025 (expected)</div>
+            </div>
+        </h1>
+        </div>
+    </div>
+
+    <div class="flex flex-col justify-center mt-36">
+        <div class="w-10 text-center ml-44 font-semibold">Resume</div>
+        <div class="flex justify-center">
+        <h1 class="intro text-2xl font-bold pt-10 mr-10 backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl p-5 rounded-lg">
+            My resume
+            <div class="flex mt-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right text-orange-600 mt-0.5"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>
+                <a href="https://drive.google.com/file/d/1PFInKluGJVMn6KbqJ1Qwok0wJrT0A6fj/view?usp=sharing" target="_blank" class="text-base font-semibold ml-2 hover:text-red-500">Click here to view my resume</a>
+            </div>
+        </h1>
+        <img src="profile-image-5.png" class="w-96 h-96" alt=""/>
+    </div>
     </div>
 </div>
 
@@ -15,5 +50,10 @@
     .intro{
         width: 500px;
         height: 350px;
+    }
+
+    .intro-1{
+        width: 500px;
+        height: 390px;
     }
 </style>

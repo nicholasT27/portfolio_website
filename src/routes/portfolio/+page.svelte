7bb37427b2e3da29f599932b6023695465ba1891
kee-siang <tksiang.ss@example.com>
2024-01-29 17:24:48 +0800

    create portfolio page

---
+++ src/routes/portfolio/+page.svelte
@@ -0,0 +1,81 @@
+<div class="bg-gradient-to-br from-teal-300 via-bg-indigo-400 to-purple-200">
+    <img class="h-16 w-16"src="logo.png" alt="..."/>
+    <h1 class="text-9xl font-semibold mt-20 ml-10">
+        Portfolio
+    </h1>
+
+    <h1 class="mt-24 ml-48 w-fit text-6xl font-semibold">Latest <span class="text-violet-800">Portfolio</span></h1>
+    <div class="mt-2 ml-48 border-2 border-rose-800 w-56"></div>
+    <div class="mt-5 ml-48 border-2 border-pink-700 w-48"></div>
+
+    <div class="flex">
+    <div class="mt-10 ml-48 backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl portfolio-div-1 rounded-lg p-5">
+        <h1 class="text-2xl font-bold">Next Job Website</h1>
+        <p class="text-justify font-semibold mt-2">The website is constructed using HTML, Tailwind CSS, SvelteKit, Amazon AWS and Pocketbase. 
+           It serves as a dynamic job portal, facilitating job seekers in their search for employment opportunities,
+           while employers can seamlessly post and manage job vacancies on the platform.
+        </p>
+        <div class="flex mt-5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right text-orange-600 mt-1"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>
+            <a href="https://901ed19e.portfolio-next-job.pages.dev" target="_blank" class="ml-1 font-semibold text-base mt-0.5 hover:text-red-500">Live Preview</a>
+        </div>
+    </div>
+
+    <div id="next-job-pic" class="m-2">
+        <img src="portfolio-pic-1.png" alt="..." class="h-96 ml-24 mt-10 drop-shadow-xl shadow-xl"/>
+    </div>
+    </div>
+
+    <h1 class="mt-24 ml-48 w-fit text-6xl font-semibold">Other <span class="text-violet-800">Portfolio</span></h1>
+    <div class="mt-2 ml-48 border-2 border-rose-800 w-56"></div>
+    <div class="mt-5 ml-48 border-2 border-pink-700 w-48"></div>
+
+    <div class="flex">
+        <div>
+            <img id="email-automator-pic" src="portfolio-pic-2.png" alt="..." class="ml-36 drop-shadow-xl shadow-xl mt-24"/>
+        </div>
+        <div class="ml-10 mt-10 backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl portfolio-div-1 rounded-lg p-5">
+        <h1 class="text-2xl font-bold">Email Automator</h1>
+        <p class="text-justify font-semibold mt-2">
+            This application is built on Node.js and utilizes Nodemailer to handle email reception and automate the sending 
+            of auto-reply emails to recipients.
+        </p>
+        <div class="flex mt-5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right text-orange-600 mt-1"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>
+            <a href="https://901ed19e.portfolio-next-job.pages.dev" target="_blank" class="ml-1 font-semibold text-base mt-0.5 hover:text-red-500">Live Preview</a>
+        </div>
+    </div>
+    </div>
+
+    <div class="flex">
+        <div class="ml-36 mt-20 backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl portfolio-div-1 rounded-lg p-5">
+        <h1 class="text-2xl font-bold">Luno Premium</h1>
+        <p class="text-justify font-semibold mt-2">
+            This command-line application is developed using Node.js, integrating Luno
+            and Binance APIs to console logs out the price difference between Luno and Binance of BTC.
+        </p>
+        <div class="flex mt-5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right text-orange-600 mt-1"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>
+            <a href="https://901ed19e.portfolio-next-job.pages.dev" target="_blank" class="ml-1 font-semibold text-base mt-0.5 hover:text-red-500">Live Preview</a>
+        </div>
+        </div>
+        <div>
+            <img id="email-automator-pic" src="portfolio-pic-2.png" alt="..." class="ml-10 drop-shadow-xl shadow-xl mt-32"/>
+        </div>
+    </div>
+</div>
+
+<style>
+    .portfolio-div-1 {
+        width: 500px;
+        height: 390px;
+    }
+
+    #next-job-pic{
+        width: 600px;
+    }
+
+    #email-automator-pic{
+        width: 500px;
+    }
+</style>

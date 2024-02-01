efc6cda4c22dd95fcda530db56e65db62abe40d3
kee-siang <tksiang.ss@example.com>
2024-02-01 23:20:26 +0800

    create a responsive layout design for portfolio page

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,126 +1,180 @@
 <div class="bg-gradient-to-br from-teal-300 via-bg-indigo-400 to-purple-200">
-    <img class="h-16 w-16"src="logo.png" alt="..."/>
-    <h1 class="text-9xl font-semibold mt-20 ml-10" id="portfolio-page-title">
-        Portfolio
-    </h1>
+	<img class="h-16 w-16" src="logo.png" alt="..." />
+	<h1 class="text-6xl md:text-9xl font-semibold mt-20 lg:ml-10 ml-2" id="portfolio-page-title">
+		Portfolio
+	</h1>
 
-    <h1 class="mt-24 ml-48 w-fit text-6xl font-semibold">Latest <span class="text-violet-800">Portfolio</span></h1>
-    <div class="mt-2 ml-48 border-2 border-rose-800 w-56"></div>
-    <div class="mt-5 ml-48 border-2 border-pink-700 w-48"></div>
+	<h1 class="mt-24 lg:ml-48 ml-2 w-fit text-3xl md:text-6xl font-semibold">
+		Latest <span class="text-violet-800">Portfolio</span>
+	</h1>
+	<div class="mt-2 ml-2 lg:ml-48 border-2 border-rose-800 w-56" />
+	<div class="mt-5 ml-2 lg:ml-48 border-2 border-pink-700 w-48" />
 
-    <div class="flex">
-    <div class="mt-10 ml-48 backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl portfolio-div-1 rounded-lg p-5 slideRight">
-        <h1 class="text-2xl font-bold">Next Job Website</h1>
-        <p class="text-justify font-semibold mt-2">The website is constructed using HTML, Tailwind CSS, SvelteKit, Amazon AWS and Pocketbase. 
-           It serves as a dynamic job portal, facilitating job seekers in their search for employment opportunities,
-           while employers can seamlessly post and manage job vacancies on the platform.
-        </p>
-        <div class="flex mt-5">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right text-orange-600 mt-1"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>
-            <a href="https://901ed19e.portfolio-next-job.pages.dev" target="_blank" class="ml-1 font-semibold text-base mt-0.5 hover:text-red-500">Live Preview</a>
-        </div>
-    </div>
+	<div class="flex flex-col lg:flex-row p-2">
+		<div
+			class="mt-10 lg:ml-24 lg:max-w-lg lg:max-h-96 lg:mt-36 backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl rounded-lg p-5 slideRight"
+		>
+			<h1 class="text-2xl font-bold">Next Job Website</h1>
+			<p class="text-justify font-semibold mt-2=">
+				The website is constructed using HTML, Tailwind CSS, SvelteKit, Amazon AWS and Pocketbase.
+				It serves as a dynamic job portal, facilitating job seekers in their search for employment
+				opportunities, while employers can seamlessly post and manage job vacancies on the platform.
+			</p>
+			<div class="flex mt-5">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="lucide lucide-arrow-right text-orange-600 mt-1"
+					><path d="M5 12h14" /><path d="m12 5 7 7-7 7" /></svg
+				>
+				<a
+					href="https://901ed19e.portfolio-next-job.pages.dev"
+					target="_blank"
+					class="ml-1 font-semibold text-base mt-0.5 hover:text-red-500">Live Preview</a
+				>
+			</div>
+		</div>
 
-    <div id="next-job-pic" class="m-2">
-        <img src="portfolio-pic-1.png" alt="..." class="h-96 ml-10 mt-10 drop-shadow-xl shadow-xl slideLeft"/>
-    </div>
-    </div>
+		<div class="m-2 flex justify-center w-screen">
+			<img
+				src="portfolio-pic-1.png"
+				alt="..."
+				class="w-4/6 lg:w-5/6 md:mt-10 mt-4 drop-shadow-xl shadow-xl slideLeft"
+			/>
+		</div>
+	</div>
 
-    <h1 class="mt-24 ml-48 w-fit text-6xl font-semibold">Other <span class="text-violet-800">Portfolio</span></h1>
-    <div class="mt-2 ml-48 border-2 border-rose-800 w-56"></div>
-    <div class="mt-5 ml-48 border-2 border-pink-700 w-48"></div>
+	<h1 class="mt-24 ml-2 lg:ml-44 w-fit text-3xl md:text-6xl font-semibold">
+		Other <span class="text-violet-800">Portfolio</span>
+	</h1>
+	<div class="mt-2 ml-2 lg:ml-48 border-2 border-rose-800 w-56" />
+	<div class="mt-5 ml-2 lg:ml-48 border-2 border-pink-700 w-48" />
 
-    <div class="flex p-10">
-        <img id="email-automator-pic" src="portfolio-pic-2.png" alt="..." class="ml-24 mb-10 drop-shadow-xl shadow-xl mt-24 slideRight"/>
-        <div class="ml-5 mt-10 backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl portfolio-div-1 rounded-lg p-5 slideLeft">
-           
-            <h1 class="text-2xl font-bold">Email Automator</h1>
-            <p class="text-justify font-semibold mt-2">
-                This application is built on Node.js and utilizes Nodemailer to handle email reception and automate the sending 
-                of auto-reply emails to recipients.
-            </p>
-            <div class="flex mt-5">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right text-orange-600 mt-1"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>
-                <a href="https://901ed19e.portfolio-next-job.pages.dev" target="_blank" class="ml-1 font-semibold text-base mt-0.5 hover:text-red-500">Live Preview</a>
-            </div>
-    </div>
-    </div>
+	<div class="flex flex-col lg:flex-row p-2">
+		<div class="flex justify-center w-full">
+			<img
+				src="portfolio-pic-2.png"
+				alt="..."
+				class="lg:mb-10 mb-2 drop-shadow-xl shadow-xl md:mt-24 slideRight mt-5 w-5/6"
+			/>
+		</div>
+		<div
+			class="lg:max-w-md w-fit lg:mr-5 lg:mt-10 mt-2 backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl rounded-lg p-5 slideLeft"
+		>
+			<h1 class="text-2xl font-bold">Email Automator</h1>
+			<p class="text-justify font-semibold mt-2">
+				This application is built on Node.js and utilizes Nodemailer to handle email reception and
+				automate the sending of auto-reply emails to recipients.
+			</p>
+			<div class="flex mt-5">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="lucide lucide-arrow-right text-orange-600 mt-1"
+					><path d="M5 12h14" /><path d="m12 5 7 7-7 7" /></svg
+				>
+				<a
+					href="https://901ed19e.portfolio-next-job.pages.dev"
+					target="_blank"
+					class="ml-1 font-semibold text-base mt-0.5 hover:text-red-500">Live Preview</a
+				>
+			</div>
+		</div>
+	</div>
 
-    <div class="flex p-10">
-        <div class="ml-24 mt-20 backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl portfolio-div-1 rounded-lg p-5 slideRight">
-        <h1 class="text-2xl font-bold">Luno Premium</h1>
-        <p class="text-justify font-semibold mt-2">
-            This command-line application is developed using Node.js, integrating Luno
-            and Binance APIs to console logs out the price difference between Luno and Binance of BTC.
-        </p>
-        <div class="flex mt-5">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right text-orange-600 mt-1"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>
-            <a href="https://901ed19e.portfolio-next-job.pages.dev" target="_blank" class="ml-1 font-semibold text-base mt-0.5 hover:text-red-500">Live Preview</a>
-        </div>
-        </div>
-        <div class="wrapper">
-            <img id="email-automator-pic" src="portfolio-pic-2.png" alt="..." class="ml-5 drop-shadow-xl shadow-xl mt-32 border border-black slideLeft"/>
-        </div>
-        </div>
+	<div class="flex flex-col lg:flex-row p-2 pb-28">
+		<div
+			class="lg:max-w-md lg:ml-10 mt-20 backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl rounded-lg p-5 slideRight"
+		>
+			<h1 class="text-2xl font-bold">Luno Premium</h1>
+			<p class="text-justify font-semibold mt-2">
+				This command-line application is developed using Node.js, integrating Luno and Binance APIs
+				to console logs out the price difference between Luno and Binance of BTC.
+			</p>
+			<div class="flex mt-5">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="lucide lucide-arrow-right text-orange-600 mt-1"
+					><path d="M5 12h14" /><path d="m12 5 7 7-7 7" /></svg
+				>
+				<a
+					href="https://901ed19e.portfolio-next-job.pages.dev"
+					target="_blank"
+					class="ml-1 font-semibold text-base mt-0.5 hover:text-red-500">Live Preview</a
+				>
+			</div>
+		</div>
+		<div class="lg:ml-5 lg:mt-20 mt-4 flex justify-center lg:mr-5">
+			<img src="portfolio-pic-2.png" alt="..." class="drop-shadow-xl shadow-xl" />
+		</div>
+	</div>
 </div>
 
 <style>
-    .portfolio-div-1 {
-        width: 500px;
-        height: 390px;
-    }
-
-    #next-job-pic{
-        width: 600px;
-    }
-
-    #email-automator-pic{
-        width: 500px;
-    }
-
-    @keyframes text-clip {
-  from {
-    clip-path: polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%);
-  }
-  to {
-    clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
-  }
-}
-
-#portfolio-page-title{
-    animation: text-clip 1s;
-}
-
-@keyframes slide-to-right {
-	0% {
-		opacity: 0;
-		transform: translateX(-100%);
+	@keyframes text-clip {
+		from {
+			clip-path: polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%);
+		}
+		to {
+			clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+		}
 	}
 
-	100% {
-		opacity: 1;
-		transform: translateX(0);
-	}
-}
-
-.slideRight {
-	animation: slide-to-right 2s;
-}
-
-@keyframes slide-to-left {
-	0% {
-		opacity: 0;
-		transform: translateX(20%);
+	#portfolio-page-title {
+		animation: text-clip 1s;
 	}
 
-	100% {
-		opacity: 1;
-		transform: translateX(0);
+	@keyframes slide-to-right {
+		0% {
+			opacity: 0;
+			transform: translateX(-100%);
+		}
+
+		100% {
+			opacity: 1;
+			transform: translateX(0);
+		}
 	}
-}
 
-.slideLeft {
-	animation: slide-to-left 2s;
-}
+	.slideRight {
+		animation: slide-to-right 2s;
+	}
 
+	@keyframes slide-to-left {
+		0% {
+			opacity: 0;
+			transform: translateX(20%);
+		}
+
+		100% {
+			opacity: 1;
+			transform: translateX(0);
+		}
+	}
+
+	.slideLeft {
+		animation: slide-to-left 2s ease;
+	}
 </style>

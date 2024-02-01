ac6be8952580bf2c795fea974db9563032611647
kee-siang <tksiang.ss@example.com>
2024-02-01 15:48:16 +0800

    change to responsive button and navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -31,21 +31,21 @@
 
 <slot />
 
-<div class="fixed bottom-24 w-full">
+<div id="navBar"class="fixed bottom-24 w-full">
 <div class="flex justify-center items-end h-10 mt-8 absolute inline-block slide-up w-full z-30 bg-transparent">
         <div class="rounded-s-full rounded-e-full backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl font-semibold grid grid-cols-4">
-            <button class="text-center p-4 text-lg hover:bg-gradient-to-br from-indigo-200 via-fuchsia-200 to-blue-200 rounded-s-full">Home</button>
-            <button class="text-center p-4 text-lg hover:bg-gradient-to-br from-teal-300 via-bg-indigo-400 to-purple-200">Portfolio</button>
-            <button class="text-center p-4 text-lg hover:bg-gradient-to-br from-yellow-200 via-rose-300 to-orange-200">About</button>
-            <button class="text-center p-4 text-lg hover:bg-gradient-to-br from-blue-200 via-sky-300 to-blue-400 rounded-e-full">Contact</button>
+            <a class="button text-center p-4 text-lg hover:bg-gradient-to-br from-indigo-200 via-fuchsia-200 to-blue-200 rounded-s-full" href="/">Home</a>
+            <a class="button text-center p-4 text-lg hover:bg-gradient-to-br from-teal-300 via-bg-indigo-400 to-purple-200" href="/portfolio">Portfolio</a>
+            <a class="button text-center p-4 text-lg hover:bg-gradient-to-br from-yellow-200 via-rose-300 to-orange-200" href="about">About</a>
+            <a class="button text-center p-4 text-lg hover:bg-gradient-to-br from-blue-200 via-sky-300 to-blue-400 rounded-e-full" href="contact">Contact</a>
         </div>
 
 
         <audio src="background-music.mp3" loop bind:this={audio}></audio>
 
-        <div class="flex mb-14 mt-2">
+        <div class="flex mb-14 mt-2 border border-black">
         <div class="hs-tooltip">
-         <button on:click={togglePlayPause} class="hs-tooltip-toggle absolute right-24 p-2 rounded-lg bg-white/60 drop-shadow-xl shadow-xl">
+         <button id="music-button" on:click={togglePlayPause} class="hs-tooltip-toggle absolute lg:right-24 p-2 rounded-lg bg-white/60 drop-shadow-xl shadow-xl">
             {#if $play}
             <svg xmlns="http://www.w3.org/2000/svg" width="40px" height="40px" viewBox="-0.5 0 25 25" fill="none">
                 <path d="M12.5493 4.50005C11.3193 4.04005 8.70926 5.49996 6.54926 7.40996H4.94922C3.88835 7.40996 2.87093 7.83145 2.12079 8.58159C1.37064 9.33174 0.949219 10.3491 0.949219 11.41V13.41C0.949219 14.4708 1.37064 15.4883 2.12079 16.2385C2.87093 16.9886 3.88835 17.41 4.94922 17.41H6.54926C8.65926 19.35 11.2693 20.78 12.5493 20.33C14.6493 19.55 14.9992 15.33 14.9992 12.41C14.9992 9.48996 14.6493 5.28005 12.5493 4.50005Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -69,7 +69,7 @@
         </div>
 
         <div class="hs-tooltip">
-        <button on:click={backToTop} class="bg-white/60 drop-shadow-xl shadow-xl absolute right-6 p-3 rounded-lg hs-tooltip-toggle">
+        <button id="backToTop" on:click={backToTop} class="bg-white/60 drop-shadow-xl shadow-xl absolute lg:right-6 p-3 rounded-lg hs-tooltip-toggle">
             <svg xmlns="http://www.w3.org/2000/svg" width="32px" height="32px" fill="currentColor" class="bi bi-arrow-bar-up" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M8 10a.5.5 0 0 0 .5-.5V3.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 3.707V9.5a.5.5 0 0 0 .5.5m-7 2.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5"/>
             </svg>
@@ -78,10 +78,88 @@
             back to top
         </span>
         </div>
-        </div>
-
-       
-
-        
+    </div>
     </div>
 </div>
+
+<style>
+    @media(min-width: 270px) and (max-width: 350px) {
+        
+        #music-button{
+            position:absolute;
+            right: 8px;
+            bottom: 150px;
+        }
+
+        #backToTop{
+            position: absolute;
+            right: 8px;
+            bottom: 80px;
+        }
+
+        .button{
+            font-size: 14px;
+            padding: 10px;
+        }
+
+        #navBar{
+            bottom: 80px;
+        }
+    }
+
+    @media(min-width: 360px) and (max-width: 400px) {
+        
+        #music-button{
+            position:absolute;
+            right: 8px;
+            bottom: 150px;
+        }
+
+        #backToTop{
+            position: absolute;
+            right: 8px;
+            bottom: 80px;
+        }
+    }
+
+    @media(min-width: 410px) and (max-width: 500px) {
+        
+        #music-button{
+            position:absolute;
+            right: 8px;
+            bottom: 150px;
+        }
+
+        #backToTop{
+            position: absolute;
+            right: 8px;
+            bottom: 80px;
+        }
+
+        .button{
+            font-size: 20px;
+        }
+
+    }
+
+    @media (min-width: 700px) and (max-width: 1000px){
+        .button{
+            font-size: 30px;
+            padding: 20px;
+        }
+
+        #music-button{
+            position:absolute;
+            right: 8px;
+            bottom: 150px;
+        }
+
+        #backToTop{
+            position: absolute;
+            right: 8px;
+            bottom: 80px;
+        }
+        }
+
+   
+</style>

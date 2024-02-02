106a1971f1eab127bd1cd588eff2e0e8d3cd3259
kee-siang <tksiang.ss@example.com>
2024-02-02 14:41:21 +0800

    change resume url

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+  import { PUBLIC_GOOGLE_DRIVE_URL } from '$env/static/public';
+</script>
+
 
 <div class="bg-gradient-to-br from-orange-200 via-rose-300 to-yellow-200 h-full">
     <img class="h-16 w-16"src="logo.png" alt="..."/>
@@ -45,7 +49,7 @@
             My resume
             <div class="flex mt-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right text-orange-600 mt-0.5"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>
-                <a href="https://drive.google.com/file/d/1PFInKluGJVMn6KbqJ1Qwok0wJrT0A6fj/view?usp=sharing" target="_blank" class="text-base font-semibold ml-2 hover:text-red-500">Click here to view my resume</a>
+                <a href={PUBLIC_GOOGLE_DRIVE_URL} target="_blank" class="text-base font-semibold ml-2 hover:text-red-500">Click here to view my resume</a>
             </div>
         </h1>
         </div>

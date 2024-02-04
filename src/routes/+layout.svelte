750cb19d85d89b302f2d0c5dd1790b82fda80702
kee-siang <tksiang.ss@example.com>
2024-02-04 18:29:29 +0800

    cancel tooltip for backToTop button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -82,16 +82,16 @@
         </span>
         </div> -->
 
-        <div class="hs-tooltip">
+        <!-- <div class="hs-tooltip"> -->
         <button id="backToTop" on:click={backToTop} class="bg-white/60 drop-shadow-xl shadow-xl absolute lg:right-6 p-3 rounded-lg hs-tooltip-toggle">
             <svg xmlns="http://www.w3.org/2000/svg" width="32px" height="32px" fill="currentColor" class="bi bi-arrow-bar-up" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M8 10a.5.5 0 0 0 .5-.5V3.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 3.707V9.5a.5.5 0 0 0 .5.5m-7 2.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5"/>
             </svg>
         </button>
-        <span class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-gray-900 text-white rounded-lg drop-shadow-xl shadow-xl" role="tooltip">
+        <!-- <span class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-gray-900 text-white rounded-lg drop-shadow-xl shadow-xl" role="tooltip">
             back to top
         </span>
-        </div>
+        </div> -->
     </div>
     </div>
 </div>

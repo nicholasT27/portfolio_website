e2e72d5b5d4f2b3a1da973ab6c510367c9c9246e
kee-siang <tksiang.ss@example.com>
2024-02-04 16:22:29 +0800

    preline configuration

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+    rollupOptions: {
+      input: {
+        main: 'src/main.js',
+        vendor: 'node_modules/preline/dist/preline.js'
+      }
+    }
+  }
 });

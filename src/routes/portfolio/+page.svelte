eaad97f688ee7cc3230d6c724eb0e740195a3a1a
kee-siang <tksiang.ss@example.com>
2024-03-03 16:57:02 +0800

    add a new section for nphone repair landing page

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,28 +1,33 @@
 <script>
-import { onMount } from 'svelte';
+	import { onMount } from 'svelte';
 
-  let emailDivision;
-  let lunoDivision;
+	let wordpressDiv;
+	let emailDivision;
+	let lunoDivision;
 
-  onMount ( () => {
-    emailDivision = document.getElementById("emailDiv");
-    lunoDivision = document.getElementById("lunoDiv");
-  })
-  
-  const scrollFunc = () => {
-     const y = window.scrollY;
-     console.log(y)
-     if( y >= 400 ) {
-       emailDivision.style.display = "flex";
-     }
-     
-     if ( y >= 1000 ) {
-      lunoDivision.style.display = "flex";
-     }
-  }
+	onMount(() => {
+		wordpressDiv = document.getElementById('wordpress-div');
+		emailDivision = document.getElementById('emailDiv');
+		lunoDivision = document.getElementById('lunoDiv');
 
-  window.addEventListener("scroll", scrollFunc);
+	});
 
+	const scrollFunc = () => {
+		const y = window.scrollY;
+		if (y >= 400) {
+			wordpressDiv.style.display = 'flex';
+		}
+
+		if (y >= 1000) {
+			emailDivision.style.display = 'flex';
+		}
+
+		if (y >= 1800){
+			lunoDivision.style.display = 'flex';
+		}
+	};
+
+	window.addEventListener('scroll', scrollFunc);
 </script>
 
 <svelte:head>
@@ -41,15 +46,64 @@ import { onMount } from 'svelte';
 	<div class="mt-2 ml-2 lg:ml-48 border-2 border-rose-800 w-56" />
 	<div class="mt-5 ml-2 lg:ml-48 border-2 border-pink-700 w-48" />
 
-	<div class="flex flex-col justify-center lg:flex-row w-auto">
+	<div class="flex flex-col">
+		<section class="flex flex-col lg:flex-row justify-center items-center">
+			<div
+				class="mt-10 lg:max-w-lg lg:h-96 backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl rounded-lg p-5 slideRight"
+			>
+				<h1 class="text-2xl font-bold text-red-600">Next Job Website</h1>
+				<p class="text-justify font-semibold mt-2=">
+					The website is constructed using HTML, CSS, Javascript, Flowbite, Daisy UI, Tailwind CSS,
+					SvelteKit, Amazon AWS and Pocketbase. It serves as a dynamic job portal, facilitating job
+					seekers in their search for employment opportunities, while employers can seamlessly post
+					and manage job vacancies on the platform.
+				</p>
+				<div class="flex mt-5">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						class="lucide lucide-arrow-right text-orange-600 mt-1"
+						><path d="M5 12h14" /><path d="m12 5 7 7-7 7" /></svg
+					>
+					<a
+						href="https://portfolio-next-job.pages.dev/"
+						target="_blank"
+						class="ml-1 font-semibold text-base mt-0.5 hover:text-red-500">Live Preview</a
+					>
+				</div>
+			</div>
+
+			<div class="m-5 lg:w-4/12 flex justify-center">
+				<img
+					src="portfolio-pic-1.png"
+					alt="..."
+					class="lg:mt-0 drop-shadow-xl shadow-xl slideLeft"
+				/>
+			</div>
+		</section>
+
+		<section>
+			<div id="wordpress-div" class="flex flex-col justify-center lg:flex-row items-center">
+		<div class="m-5 lg:w-4/12 flex justify-center">
+			<img
+				src="portfolio-pic-2.png"
+				alt="..."
+				class="lg:mb-10 mb-2 drop-shadow-xl shadow-xl md:mt-24 slideRight"
+			/>
+		</div>
 		<div
-			class="mt-10 lg:max-w-lg lg:max-h-96 lg:mt-16 backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl rounded-lg p-5 slideRight"
+			class="slideLeft lg:h-96 lg:max-w-md w-full lg:mr-5 lg:mt-10 mt-2 backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl rounded-lg p-5"
 		>
-			<h1 class="text-2xl font-bold">Next Job Website</h1>
-			<p class="text-justify font-semibold mt-2=">
-				The website is constructed using HTML, CSS, Javascript, Flowbite, Daisy UI, Tailwind CSS, SvelteKit, Amazon AWS and Pocketbase.
-				It serves as a dynamic job portal, facilitating job seekers in their search for employment
-				opportunities, while employers can seamlessly post and manage job vacancies on the platform.
+			<h1 class="text-2xl font-bold text-orange-600">NPhone & Laptop Repair Landing Page</h1>
+			<p class="text-justify font-semibold mt-2">
+				A simple landing page portfolio built by Wordpress and Elementor.
 			</p>
 			<div class="flex mt-5">
 				<svg
@@ -66,16 +120,14 @@ import { onMount } from 'svelte';
 					><path d="M5 12h14" /><path d="m12 5 7 7-7 7" /></svg
 				>
 				<a
-					href="https://portfolio-next-job.pages.dev/"
+					href="https://nphonerepair.com/"
 					target="_blank"
 					class="ml-1 font-semibold text-base mt-0.5 hover:text-red-500">Live Preview</a
 				>
 			</div>
 		</div>
-
-		<div class="m-5 lg:w-4/12 flex justify-center">
-			<img src="portfolio-pic-1.png" alt="..." class="lg:mt-0 drop-shadow-xl shadow-xl slideLeft" />
-		</div>
+	</div>
+		</section>
 	</div>
 
 	<h1 class="mt-24 ml-2 lg:ml-44 w-fit text-3xl md:text-6xl font-semibold">
@@ -84,18 +136,18 @@ import { onMount } from 'svelte';
 	<div class="mt-2 ml-2 lg:ml-48 border-2 border-rose-800 w-56" />
 	<div class="mt-5 ml-2 lg:ml-48 border-2 border-pink-700 w-48" />
 
-	<div id="emailDiv" class="flex flex-col justify-center lg:flex-row">
+	<div id="emailDiv" class="flex flex-col justify-center lg:flex-row items-center">
 		<div class="m-5 lg:w-5/12 flex justify-center lg:justify-start">
 			<img
-				src="portfolio-pic-2.png"
+				src="portfolio-pic-3.png"
 				alt="..."
 				class="lg:mb-10 mb-2 drop-shadow-xl shadow-xl md:mt-24 slideRight"
 			/>
 		</div>
 		<div
-			class="slideLeft lg:max-w-md w-fit lg:mr-5 lg:mt-10 mt-2 backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl rounded-lg p-5"
+			class="slideLeft lg:h-96 lg:max-w-md w-fit lg:mr-5 lg:mt-12 mt-2 backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl rounded-lg p-5"
 		>
-			<h1 class="text-2xl font-bold">Email Automator</h1>
+			<h1 class="text-2xl font-bold text-teal-500">Email Automator</h1>
 			<p class="text-justify font-semibold mt-2">
 				This application is built on Node.js, Nodemailer, IMAP to monitor inbox, detects new email
 				and sends automated replies based on predefined script.
@@ -127,10 +179,11 @@ import { onMount } from 'svelte';
 		<div
 			class="lg:max-w-md mt-20 backdrop-blur-lg bg-white/60 drop-shadow-xl shadow-xl rounded-lg p-5 slideRight"
 		>
-			<h1 class="text-2xl font-bold">Luno Premium</h1>
+			<h1 class="text-2xl font-bold text-lime-600">Luno Premium</h1>
 			<p class="text-justify font-semibold mt-2">
 				This command-line application is developed using Node.js, integrating Luno and Binance APIs
-				to console logs out the price difference between Luno and Binance for different cryptocurrency.
+				to console logs out the price difference between Luno and Binance for different
+				cryptocurrency.
 			</p>
 			<div class="flex mt-5">
 				<svg
@@ -154,17 +207,25 @@ import { onMount } from 'svelte';
 			</div>
 		</div>
 		<div class="lg:mt-24 mt-4 lg:w-5/12 flex justify-center m-5">
-			<img src="portfolio-pic-3.png" alt="..." class="drop-shadow-xl shadow-xl slideLeft" />
+			<img src="portfolio-pic-4.png" alt="..." class="drop-shadow-xl shadow-xl slideLeft" />
 		</div>
 	</div>
 </div>
 
 <style>
-	#emailDiv{
+	h1{
+		font-family: 'Young Serif', serif;
+	}
+
+	#wordpress-div{
 		display: none;
 	}
 
-	#lunoDiv{
+	#emailDiv {
+		display: none;
+	}
+
+	#lunoDiv {
 		display: none;
 	}
 
@@ -197,14 +258,13 @@ import { onMount } from 'svelte';
 		animation: slide-to-right 2s;
 	}
 
-
 	@keyframes slide-to-left {
 		0% {
-                transform: translateX(100%);
-            }
-        100% {
-                transform: translateX(0);
-            }
+			transform: translateX(100%);
+		}
+		100% {
+			transform: translateX(0);
+		}
 	}
 
 	.slideLeft {

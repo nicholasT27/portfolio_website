5e7fb385895ca88a4d1ae1698fcb6e601e122b3c
kee-siang <tksiang.ss@example.com>
2024-03-11 19:31:53 +0800

    change home layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,19 +23,20 @@
 <div class="bg-gradient-to-br from-indigo-200 via-fuchsia-200 to-blue-200 h-screen">
     <img id="logo" class="h-16 w-16"src="logo.png" alt="..."/>
     <div id="title-div" class="flex slide-down">
-        <h1 id="title" class="h-fit text-center md:ml-10 text-4xl lg:my-16 lg:ml-64 font-bold">
+        <h1 id="title" class="h-fit text-center md:ml-10 text-3xl lg:my-16 lg:ml-64 font-bold">
             Hi My name is Siang. A passionate young <span class="text-violet-800">
             web developer</span> that loves <span class="text-indigo-800">creating</span>, 
             <span class="text-fuchsia-800">imaginating</span> and <span class="text-pink-700">coding things</span> 
             to make them <span class="text-rose-800">real, accessible.</span></h1>
         <div id="image-div">
-            <img class="lg:mr-10 lg:h-96 lg:w-96" src="profile-image.png" alt="..."/>
+            <img class="lg:mr-10 lg:h-96 lg:w-full" src="sayhi.gif" alt="..."/>
         </div>
     </div>
 </div>
 
 <style>
     #title{
+        font-family: "Verdana", sans-serif;
         width: 400px;
     }
 
